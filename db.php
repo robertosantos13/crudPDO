5b611234d0147564bc36cec679314d4a608dfe26
robertosantos13 <osrbeto@gmail.com>
2018-11-21 00:50:33 -0200

    adicioando modal para adicionar - deletar e editar com atualizar

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'connection.php';
-
+$id = 0;
 // exibir
 try {
     $stmt = $conn->prepare("SELECT * FROM cadastro");
@@ -11,10 +11,64 @@ try {
    
 
 } catch(PDOException $e) {
-    echo 'Error ao listar o banco ' . $e.getMessage();
+    echo 'Error ao listar o banco ' . $e->getMessage();
 }
 
-// deletar
+// insert
+if(isset($_POST['salvar'])){
+    try {
 
+        $nome = $_POST['cadastro_nome'];
+        $email = $_POST['cadastro_email'];
+        $cidade = $_POST['cadastro_cidade'];
+
+        $sql = "INSERT INTO cadastro(cadastro_nome, cadastro_email, cadastro_cidade)
+                    VALUES ('$nome','$email','$cidade')";
+        $conn->exec($sql);
+        
+        header('location: index.php?insert_success');
+        
+    } catch(PDOException $e) {
+        echo 'Erro ao tentar salvar os dados!' .$e->getMessage();    
+    }
+}
+
+
+// deletar
+if(isset($_GET['delete'])){
+ 
+    try {
+       
+        $id = $_GET['delete'];
+        $sql = "DELETE FROM cadastro WHERE cadastro_id='$id'";
+        $conn->exec($sql);
+
+        header('location: index.php?deleted_success');
+    } catch(PDOException $e) {
+        echo 'Erro ao tentar deletar os dados!' .$e->getMessage();    
+    }
+}
 
 // update
+if(isset($_POST['cadastro_nome'])){
+    try
+     {
+
+    $id = $_POST['cadastro_id'];
+    $nome = $_POST['cadastro_nome'];
+    $email = $_POST['cadastro_email'];
+    $cidade = $_POST['cadastro_cidade'];
+
+    $sql = "UPDATE cadastro SET cadastro_nome='$nome', cadastro_email='$email', cadastro_cidade='$cidade' WHERE cadastro_id=$id";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    
+    header('location: index.php?updated_success');
+    }
+    catch(PDOException $e)
+    {
+        echo $sql . $e->getMessage();
+    }
+    $conn = null;
+}
--- index.php
+++ index.php
@@ -1,108 +1,234 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <title>Crud com PDO</title>
 </head>
+
 <body>
-<?php require_once 'db.php' ?>
-    <div class="container mt-3">
-    <h2 class="text-center">Crud com PDO e Bootstrap 4 </h2> <hr>
-    <div class="float-right">
-        <button type="button" id="btnOpenModal" class="btn btn-success">Adicionar</button>
-    </div>
+    <?php require_once 'db.php';
+    
+?>
 
-    <!-- adicionando modal -->
-    <div class="modal fade" id="myModal">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <!-- header -->
-                <div class="modal-header">
-                    <h4 class="modal-title">Cadastro de Usuários</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
+        <div class="container">
 
-                <!-- body/content -->
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="nome">Nome:</label>
-                        <input type="text" nam="nome" class="form-control" placeholder="Digite nome">
-                    </div>
-                    <div class="form-group">
-                        <label for="nome">Email:</label>
-                        <input type="emal" name="email" class="form-control" placeholder="Digite nome">
-                    </div>
-                    <div class="form-group">
-                        <label for="cidade">Cidade</label>
-                        <select class="custom-select" name="cidade" id="cidade">
-                            <option selected>Escolha a cidade...</option>
-                            <option value="">Belo Horizonte</option>
-                            <option value="">Betim</option>
-                            <option value="">Contagem</option>
-                            <option value="">Esmeraldas</option>
-                            <option value="">Ibirite</option>                           
-                        </select>
-                    </div>                
-                </div>
-                <div class="modal-footer mb-2">
-                    <button type="button" class="btn btn-success">Adicionar</button>
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+            <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                <div class="card-header">Painel de Usuários</div>
+                <div class="card-body">
+                    <h5 class="card-title">Crud PDO com Bootstrap</h5>
+                    <p class="card-text">Este é apenas um Crud PDO Básico.</p>
                 </div>
             </div>
-        </div>
+
+            <div class="float-right mb-2">
+                <a class="btn btn-primary btn-md" href="#" role="button" id="btnopenmodal"  data-toggle="modal" data-target="#cadastroModal">Adicionar</a>
+            </div>
+            <!-- Modal cadastro -->
+<div class="modal fade" id="cadastroModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Cadastro Usuário</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="db.php" method="POST">
+            <div class="form-group">
+                <label for="cadastro_nome">Nome</label>
+                <input type="text" class="form-control" name="cadastro_nome" placeholder="Digite nome">
+            </div>
+            <div class="form-group">
+                <label for="cadastro_email">Email</label>
+                <input type="text" class="form-control" name="cadastro_email" placeholder="Digite email">
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label for="cidade" class="input-group-text">Cidade</label>
+                </div>
+                <select name="cadastro_cidade" id="cadastro_cidade" class="custom-select">
+                    <option selected>Escolha a cidade...</option>
+                    <option value="<?php echo (isset($cidade) ? $cidade: 'Belo Horizonte'); ?>">
+                        Belo Horizonte
+                    </option>
+                    <option value="<?php echo (isset($cidade) ? $cidade: 'Esmeraldas'); ?>">
+                        Esmeraldas
+                    </option>
+                    <option value="<?php echo (isset($cidade) ? $cidade: 'Ibirite'); ?>">
+                        Ibirite
+                    </option>
+                    <option value="<?php echo (isset($cidade) ? $cidade: 'Mario Campos'); ?>">
+                        Mario Campos
+                    </option>
+                </select>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-defalut" data-dismiss="modal">Fechar</button>
+                <button type="submit" class="btn btn-success" name="salvar">Salvar</button>
+            </div>
+        </form>
+      </div>     
     </div>
-     <!-- table -->
-     <div class="table-responsive card" >
-        <div class="card-header text-center">
-                Painel de Usuários
-        </div>
-        <table class="table table-bordered table-hover mt-3">
-            <thead class="thead-dark">
+  </div>
+</div>
+
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
                 <tr>
-                    <td>#</td>
-                    <td>Nome</td>
-                    <td>Email</td>
-                    <td>Cidade</td>
-                    <td colspan="2">Ações</td>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
-            <?php foreach($result as $row): ?>
-                <tr>
-                    <td><?= $row['cadastro_id']; ?> </td>
-                    <td><?= $row['cadastro_nome']; ?> </td>
-                    <td><?= $row['cadastro_email']; ?> </td>
-                    <td><?= $row['cadastro_cidade']; ?> </td>
-                    <td>
-                        <input type="button" class="btn btn-info btn-sm">
-                        <a href="">Editar</a>
-                    </td>
 
-                    <td>
-                        <input type="button" class="btn btn-danger btn-sm">
-                        <a href="">Deletar</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
+                <?php foreach($result as $row): ?>
+                    <tr>
+                        <td scope="row">
+                            <?= $row['cadastro_id'];?>
+                        </td>
+                        <td>
+                            <?= $row['cadastro_nome'];?>
+                        </td>
+                        <td>
+                            <?= $row['cadastro_email'];?>
+                        </td>
+                        <td>
+                            <?= $row['cadastro_cidade'];?>
+                        </td>
+                        <td>
+                            <button type="submit" class="btn btn-info" data-toggle="modal"
+                                data-target="#editarModal"
+                                data-whateveredit="<?= $row['cadastro_id'];?>"
+                                data-whatevereditnome="<?= $row['cadastro_nome'];?>"
+                                data-whatevereditemail="<?= $row['cadastro_email'];?>"
+                                data-whatevereditcidade="<?= $row['cadastro_cidade'];?>"  
+                                >
+                                Editar
+                            </button>
+                            <button type="button" class="btn btn-danger"
+                             data-toggle="modal" data-target="#exampleModal"
+                              data-whatever="<?= $row['cadastro_id'];?>">Deletar</button>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
             </tbody>
         </table>
     </div>
+</div>
 
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        Deseja mesmo deletar esse usuario de nome - <?= $row['cadastro_nome'] ?>?
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <a type="submit" class="btn btn-danger" 
+            href="db.php?delete=<?= $row['cadastro_id'];?>" name="delete" >Deletar</a>
+      </div>
     </div>
+  </div>
+</div>
+        <!-- Editar modal -->
+        <div class="modal fade" id="editarModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    Editando usuário de nome - <?= $row['cadastro_nome']; ?>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <form action="db.php" method="POST">
+                <input type="hidden" class="form-control" name="cadastro_id"  id="cadastro_id" value="<?php $id;?>" placeholder="Id do sistema">
+            <div class="form-group">
+                <label for="cadastro_nome">Nome</label>
+                <input type="text" class="form-control" name="cadastro_nome" id="cadastro_nome" placeholder="Digite nome">
+            </div>
+            <div class="form-group">
+                <label for="cadastro_email">Email</label>
+                <input type="text" class="form-control" name="cadastro_email" id="cadastro_email" placeholder="Digite email">
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label for="cidade" class="input-group-text">Cidade</label>
+                </div>
+                <select name="cadastro_cidade" id="cadastro_cidade" class="custom-select">
+                    <option selected>Escolha a cidade...</option>
+                    <option value="<?php echo (isset($cidade) ? $cidade: 'Belo Horizonte'); ?>">
+                        Belo Horizonte
+                    </option>
+                    <option value="<?php echo (isset($cidade) ? $cidade: 'Esmeraldas'); ?>">
+                        Esmeraldas
+                    </option>
+                    <option value="<?php echo (isset($cidade) ? $cidade: 'Ibirite'); ?>">
+                        Ibirite
+                    </option>
+                    <option value="<?php echo (isset($cidade) ? $cidade: 'Mario Campos'); ?>">
+                        Mario Campos
+                    </option>
+                </select>
+            </div>
+            <div class="form-group float-right">
+                 <button type="submit" class="btn btn-warning" >Alterar</button>     
+            </div>
+        </form>
+               
+            </div>
+        </div>
 
-   
-    <script>
-        $(document).ready(function() {
-            $('#btnOpenModal').click(function(){
-                $('#myModal').modal();
+       
+
+        <script>
+        $('#exampleModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget) // Button that triggered the modal
+            var recipient = button.data('whatever') // Extract info from data-* attributes
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+            var modal = $(this)
+            modal.find('.modal-title').text('New message to ' + recipient)
+            modal.find('.modal-body input').val(recipient)
+            
             });
-        });
-    </script>
+
+            $('#editarModal').on('show.bs.modal', function(event) {
+                var button = $(event.relatedTarget) // Button that triggered the modal
+                var recipientedit = button.data('whateveredit') // Extract info from data-* attributes
+                var recipienteditnome = button.data('whatevereditnome')
+                var recipienteditemail = button.data('whatevereditemail')
+                var recipienteditcidade = button.data('whatevereditcidade')
+               
+      
+                    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+                    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+                var modal = $(this)
+                modal.find('.modal-title').text('Editando ' + recipienteditnome)
+                modal.find('.modal-body input').val(recipienteditnome)
+                modal.find('#cadastro_id').val(recipientedit)
+                modal.find('#cadastro_nome').val(recipienteditnome)
+                modal.find('#cadastro_email').val(recipienteditemail)
+                modal.find('#cadastro_cidade').val(recipienteditcidade)
+            })
+        </script>
 </body>
+
 </html>
